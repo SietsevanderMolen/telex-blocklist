f9013202c6c252e7a6e8ff992a341114a3921f49
Sietse van der Molen <sietse@vdmolen.eu>
2015-06-17 22:16:24 +0200

    poc

--- blocklist.py
+++ blocklist.py
@@ -1,7 +1,9 @@
 import tgl
-from telex.utils.decorators import pm_only, group_only
-from telex import plugin
+from telex import plugin, auth
 import re
+
+class AccessError(Exception):
+    pass
 
 
 class BlocklistPlugin(plugin.TelexPlugin):
@@ -9,34 +11,67 @@ class BlocklistPlugin(plugin.TelexPlugin):
     Blocklist plugin for Telex bot.
     """
     patterns = {
-        "^$": "block"
+        "^{prefix}block$": "block",
+        "^{prefix}block (\d+)$$": "block_by_id",
+        "^{prefix}unblock (\d+)$": "unblock"
     }
 
     usage = [
-        ":s/pattern/string: substitute pattern for string in the last message"
+        "Reply to somebody's message with {prefix} to block them.",
+        "{prefix}block 12345: block by user id"
     ]
 
     def __init__(self):
         super().__init__()
+        self.blocked_user_ids = []
 
     def pre_process(self, msg):
         if not hasattr(msg, 'text'):
             return
 
-        if hasattr(msg.src, 'username'):
-            username = msg.src.username
-        else:
-            username = ""
+        # check if user addresses bot
+        for plugin_info in self.bot.plugin_manager.getAllPlugins():
+            if type(plugin_info.plugin_object.patterns) is dict:
+                for pattern, func in plugin_info.plugin_object.patterns.items():
+                    if plugin_info.plugin_object.is_activated and msg.text is not None:
+                        pattern = pattern.replace("{prefix}", self.bot.pfx)
+                        matches = re.search(pattern, msg.text)
+                        if matches is not None:
+                            uid = msg.src.id
+                            if uid in self.blocked_user_ids:
+                                raise AccessError("acces denied for {}".format(uid))
+            elif  type(plugin_info.plugin_object.patterns) is list:
+                for pattern in plugin_info.plugin_object.patterns:
+                    if plugin_info.plugin_object.is_activated and msg.text is not None:
+                        pattern = pattern.replace("{prefix}", self.bot.pfx)
+                        matches = re.search(pattern, msg.text)
+                        if matches is not None:
+                            uid = msg.src.id
+                            if uid in self.blocked_user_ids:
+                                raise AccessError("acces denied for {}".format(uid))
 
-        if msg.src.last_name:
-            name = msg.src.first_name + ' ' + msg.src.last_name
-        else:
-            name = msg.src.first_name
-
-        self.insert(msg_id=msg.id, timestamp=msg.date,
-                    uid=msg.src.id, username=username,
-                    name=name,
-                    chat_id=msg.dest.id, message=msg.text)
-
+    @auth.authorize(groups=['admins'])
     def block(self, msg, matches):
-        return ""
+        print("block called")
+        try:
+            peer = msg.reply.src
+            self.blocked_user_ids = list(set(self.blocked_user_ids + [peer.id]))
+            return peer.send_msg("you're now blocked")
+        except AttributeError:
+            return "please reply to a fresh message to block"
+
+    @auth.authorize(groups=['admins'])
+    def block_by_id(self, msg, matches):
+        print("block_by_id called")
+        uid = int(matches.group(1))
+        self.blocked_user_ids = list(set(self.blocked_user_ids + [uid]))
+        return "blocked " + str(uid) 
+
+    @auth.authorize(groups=['admins'])
+    def unblock(self, msg, matches):
+        uid = int(matches.group(1))
+        if uid in self.blocked_user_ids:
+            self.blocked_user_ids.remove(uid)
+            msg.dest.send_msg("unblocked user")
+        return
+
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 # telex-blocklist
+Reply to a message with {prefix}block to block a user from using the bot.
+
+To block user 12345:
+
+    {prefix}block 12345
+
+To unblock user 12345:
+
+    {prefix}unblock 12345 
